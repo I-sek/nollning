1542e4eda584c6ba638b670607343ac6541d9591
Erik Marforio <emarforio@gmail.com>
2019-03-12 11:59:18 +0100

    Transfer of local project to git

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# nollning
-The page used on https://isek.se/nollning
+nollning
+========
+The page used on https://nollning.isek.se
+
+## Usage
+All edits should be made here in the repo. When it is time to deploy the page, you just have to 
+folllow the steps in the deployment section. After deploying, create a "release" here on GitHub and tag it with the current year as the version (e.g. "v2019").
+
+## Deploying
+1. Clone this repo (`git clone https://github.com/Isektionen-LTH/nollning.git`)
+2. Remove the `.git` folder and the `.gitignore` file (they are hidden)
+4. Archive old site
+5. Upload to webhosting
+
+## Notes
+`weekData` is an array with week objects. This is an example of how a week object may look like:
+
+```javascript
+{
+  title: "Vecka 0",
+  days: [
+    {
+      header: "Upprop",
+      body: "Lorem ipsum dolor sit amet",
+      date: "2019-03-11",
+      geo: "55.7109989,13.2072995"
+    },
+    {
+      header: "Phaddermys",
+      body: "Lorem ipsum dolor sit amet",
+      date: "2019-03-12",
+      geo: "55.7109491,13.2064024"
+    },
+    {
+      header: "FlyING",
+      body: "Lorem ipsum dolor sit amet",
+      date: "2019-03-13",
+      geo: "55.7109491,13.2064024"
+    }
+  ]
+}
+```
---
+++ js/index.js
@@ -0,0 +1,129 @@
+// User settings
+const gasqueTime = new Date('2019-03-08 16:00');
+
+
+
+// Global constants
+// Google Maps API key (restricted for use on https://*.isek.se/* only)
+const MAPS_API_KEY = 'AIzaSyC3SinPUeSX8OirBR4mS-scUPktb7mtAUU';
+
+// Days range from 0-6 and begin on Sunday
+const dayLabels = [
+  'SÖN',
+  'MÅN',
+  'TIS',
+  'ONS',
+  'TOR',
+  'FRE',
+  'LÖR',
+]
+
+
+// Setup
+window.onload = init;
+
+// Function to run on page load
+function init() {
+  setInterval(updateTimeLeft, 1000);
+  loadWeekData();
+}
+
+// Load and insert week data
+function loadWeekData() {
+  let weekDataHtml = '';
+  
+  weekData.forEach(week => {
+    // Week start
+    weekDataHtml += 
+      `<div class="row week">
+         <div class="col">
+           <div class="row week-header">
+             <div class="col">
+               ${week.title}
+             </div>
+           </div>`;
+
+    // Days in week
+    week.days.forEach((day, index) => {
+      const date = new Date(day.date);
+      const dayLabel = dayLabels[date.getDay()];
+      const extraCss = index % 2 === 0 ? '' : 'odd';
+      weekDataHtml +=
+        `<div class="row day-header ${extraCss}" onclick="toggleDay(this)">
+           <div class="col">
+             <span class="day-label">${dayLabel}</span>
+             <span class="day-header-content">${day.header}</span>
+           </div>
+         </div>
+         <div class="row day-body">
+           <div class="col">
+             <div class="date">
+               ${dayLabel} ${date.getDate()}/${date.getMonth() + 1}
+             </div>
+             ${day.body}
+
+             <br><br>
+
+             <a href="https://maps.google.com/?q=${day.geo}" target="_blank" rel="noopener noreferrer">
+               <img class="img-fluid" alt="Visa var på Google Maps" src="https://maps.googleapis.com/maps/api/staticmap?markers=${day.geo}&size=560x200&key=${MAPS_API_KEY}">
+             </a>
+           </div>
+         </div>`;
+    });
+    
+    // Week end
+    weekDataHtml +=
+      `  </div>
+       </div>`;
+  });
+
+  // Add generated html to DOM
+  const weekDataTarget = document.getElementById('weekDataTarget');
+  weekDataTarget.innerHTML = weekDataHtml;
+}
+
+function updateTimeLeft() {
+  // Calculate time left
+  const now = new Date();
+  const timeLeftMillis = gasqueTime - now;
+  const days = Math.floor(timeLeftMillis / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeLeftMillis / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((timeLeftMillis / (1000 * 60)) % 60);
+  const seconds = Math.floor((timeLeftMillis / 1000) % 60);
+
+  // Get targets
+  const daysSegment = document.getElementById('daysSegment');
+  const hoursSegment = document.getElementById('hoursSegment');
+  const minutesSegment = document.getElementById('minutesSegment');
+  const secondsSegment = document.getElementById('secondsSegment');
+
+  // Fill targets
+  daysSegment.innerHTML = ('0' + days).slice(-2);
+  hoursSegment.innerHTML = ('0' + hours).slice(-2);
+  minutesSegment.innerHTML = ('0' + minutes).slice(-2);
+  secondsSegment.innerHTML = ('0' + seconds).slice(-2);
+}
+
+function showContent(target, num) {
+  if (target.classList.contains('selected')) return;
+
+  // Remove last selection and add to new tab
+  document.querySelector('.tab.selected').classList.remove('selected');
+  target.classList.add('selected');
+
+  // Hide all content views
+  const contentHolders = document.querySelectorAll('.content');
+  contentHolders.forEach(content => {
+    content.style.display = 'none';
+  })
+
+  // Show current content view
+  contentHolders[num].style.display = 'block';
+}
+
+function toggleDay(caller) {
+  // Target body is the next element
+  const target = caller.nextElementSibling;
+
+  target.classList.toggle('expanded');
+}
